30f22e1cbc1ce8a5bcf81e256a3e748ee0535e1a
Anurag30112003 <anuragsharma011011@gmail.com>
2024-03-16 02:24:57 +0530

    feat: read file working

--- src/app/action.ts
+++ src/app/action.ts
@@ -5,19 +5,75 @@ import { repos } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
 import anthropic from "@/lib/claude/client";
 
+interface FileContent {
+  name: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string;
+  type: string;
+  content: string;
+  encoding: string;
+  _links: {
+    self: string;
+    git: string;
+    html: string;
+  };
+}
 
+async function readFileFromGithub(repoUrl:string, filePath:string) {
+
+  const splitUrl = repoUrl.split("/");
+  const owner = splitUrl[3];
+  const repo = splitUrl[4];
+
+  const url = `https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`;
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN!}`,
+      "User-Agent": "anuragts",
+    },
+  });  
+  const fileContents = await response.json() as FileContent;
+  return fileContents.content;
+}
+
+function extractFilePaths(jsonString:string) {
+  // Define the regex pattern to match file names
+  const regexPattern = /"([^"]+)"/g;
+
+  // Extract file names using the regex pattern
+  let filePaths = [];
+  let match;
+  while ((match = regexPattern.exec(jsonString)) !== null) {
+    filePaths.push(match[1]);
+  }
+
+  // Return the extracted file paths
+  return filePaths;
+}
 
 export async function getFiles(question:string,repoUrl:string){
   const folderStructure = await getRepoStructure(repoUrl);
   const response  = await anthropic.messages.create({
-    model:'claude-3-opus-20240229',
+    model:'claude-3-opus-20240229', 
     max_tokens: 3000,
     messages: [
       {"role": "user", "content": `So you are my AI code assistant. So according to the question you have to give me the files which I need to read to make the changes in the code. here is question - ${question}, The you'll need to read to make the changes in the code. here is folder structure of the repo - ${folderStructure} , give me response in json format and only give file names I'll need to read.`},
     ],
   })
   console.log("AI response - ",response.content[0].text);
-  return response.content[0].text;
+  const links = extractFilePaths(response.content[0].text);
+  let base64Content = await readFileFromGithub(repoUrl, 'src/app/(application)/layout.tsx');
+  let content = Buffer.from(base64Content, 'base64').toString('utf8');
+
+  console.log("readFile - ",content);
+
+  console.log("links - ",links);
+  return `${content}`;
 }
 
 async function getRepoStructure(repoUrl: string) {
