5a6558329526245c68a505e1937bc3d225a09c38
Anurag30112003 <anuragsharma011011@gmail.com>
2024-03-21 19:16:21 +0530

    feat: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-primary">
-      hello
+    <main className="">
+      
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => (
-  <header className="bg-transparent text-secondary p-4 md:flex md:justify-between md:items-center">
+  <header className="bg-transparent  p-2 md:flex md:justify-between md:items-center border-b-2">
     <div className="flex justify-between items-center">
-      <h1 className="text-2xl font-bold">Logo</h1>
-      <div className="md:hidden">
-        {/* Here you can add a button for a dropdown menu when on mobile */}
+      <div className="w-14 h-14 relative">
+        <Image
+          src={"/lightlogo.webp"}
+          className="rounded-full absolute top-0 left-0"
+          layout="fill"
+          objectFit="cover"
+          alt="logo"
+        />
       </div>
     </div>
-    <nav className="hidden md:block space-x-10">
-      <a href="#home" className="text-lg">Home</a>
-      <a href="#about" className="text-lg">About</a>
-      <a href="#contact" className="text-lg">Contact</a>
-    </nav>
+    <div className="hidden md:block space-x-10">
+      <Link href="examples" className="text-lg">
+        examples
+      </Link>
+      <Link href="Login" className="text-lg bg-accent text-primary py-2 px-6 rounded-full border-2 border-accent hover:bg-primary hover:text-accent">
+        login
+      </Link>
+    </div>
   </header>
 );
 
